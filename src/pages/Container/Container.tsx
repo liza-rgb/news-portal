55c1295125bdd4faebdac355a46616e43416b824
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-05 01:56:39 +0100

    Created container for all pages

---
+++ src/pages/Container/Container.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
+
+import NavBar from "../../components/NavBar";
+
+const Container: React.FC = () => {
+  return (
+    <div className="Container">
+      <div className="mx-16 mb-20">
+        <NavBar />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Container;
