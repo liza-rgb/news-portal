84148d5d5c6be2ecdfad2065c32ebceb6302e7c2
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-05 01:57:08 +0100

    Created Page Title component

---
+++ src/components/PageTitle/PageTitle.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import BreadCrumbs from "../BreadCrumbs";
+
+interface PageTitleProps {
+  title: string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
+  return (
+    <div className="PageTitle">
+      <BreadCrumbs currentPage={title} />
+      <h1 className="text-lg pl-4 border-l-8 border-purple-main mt-4">
+        {title}
+      </h1>
+    </div>
+  );
+};
+
+export default PageTitle;
