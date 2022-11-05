51d66d218749b602bcbcc9b036b4c59baf5f623b
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-05 01:22:12 +0100

    Added default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,28 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: "Roboto, sans-serif",
+        montserrat: "Montserrat', sans-serif",
       },
       colors: {
         transparent: "#00000000",
         black: {
           main: "#2D2727",
           secondary: "#393333",
+          background: "#F8F8F8",
+          regular: "#000000",
         },
         purple: {
           main: "#7700B5",
         },
+        green: {
+          dark: "#218A61",
+          light: "#48DA86",
+        },
+      },
+      fontSize: {
+        sm: ["12px", "15px"],
+        md: ["16px", "20px"],
+        lg: ["32px", "36px"],
       },
     },
   },
