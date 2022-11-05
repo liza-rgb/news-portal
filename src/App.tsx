dfd11d9ea8551b8f9d366fb031e9b049dae31093
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-05 01:27:17 +0100

    Implemented website routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 
+import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  INDEX_ROUTE,
+  HOME_PAGE_ROUTE,
+  NEWS_PAGE_ROUTE,
+  POSTS_PAGE_ROUTE,
+  ABOUT_PAGE_ROUTE,
+  CONTACT_PAGE_ROUTE,
+} from "./config/routes";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+
+import Container from "./pages/Container";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Posts from "./pages/Posts";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Routes>
+      <Route path={INDEX_ROUTE} element={<Container />}>
+        <Route path={HOME_PAGE_ROUTE} element={<Home />} />
+        <Route path={NEWS_PAGE_ROUTE} element={<News />} />
+        <Route path={POSTS_PAGE_ROUTE} element={<Posts />} />
+        <Route path={ABOUT_PAGE_ROUTE} element={<About />} />
+        <Route path={CONTACT_PAGE_ROUTE} element={<Contacts />} />
+        <Route path={"*"} element={<Navigate replace to={HOME_PAGE_ROUTE} />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/config/routes.ts
@@ -0,0 +1,7 @@
+export const INDEX_ROUTE = "/";
+
+export const HOME_PAGE_ROUTE = "/";
+export const NEWS_PAGE_ROUTE = "/news";
+export const POSTS_PAGE_ROUTE = "/posts";
+export const ABOUT_PAGE_ROUTE = "/about";
+export const CONTACT_PAGE_ROUTE = "/contact";
