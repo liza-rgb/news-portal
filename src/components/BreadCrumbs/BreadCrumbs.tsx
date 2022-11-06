673f35ffbc13d542e9622ecd55c98ab4bc8f3f2e
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-06 02:55:17 +0100

    Updated BreadCrumbs component

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { HOME_PAGE_ROUTE } from "../../config/routes";
+import { useLocation } from "react-router-dom";
+import {
+  ABOUT_PAGE_ROUTE,
+  CONTACT_PAGE_ROUTE,
+  HOME_PAGE_ROUTE,
+  NEWS_PAGE_ROUTE,
+  POSTS_PAGE_ROUTE,
+} from "../../config/routes";
 
 import { ReactComponent as ChevronRightIcon } from "../../icons/chevron-right.svg";
 
@@ -8,17 +15,42 @@ interface BreadCrumbsProps {
 }
 
 const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ currentPage }) => {
+  const location = useLocation();
+  const routes = location.pathname.split("/");
+  console.log(routes);
+
+  const getName = (route: string) => {
+    switch (route) {
+      case HOME_PAGE_ROUTE.replace("/", ""):
+        return "Головна";
+      case NEWS_PAGE_ROUTE.replace("/", ""):
+        return "Новини";
+      case POSTS_PAGE_ROUTE.replace("/", ""):
+        return "Тексти";
+      case ABOUT_PAGE_ROUTE.replace("/", ""):
+        return "Про мене";
+      case CONTACT_PAGE_ROUTE.replace("/", ""):
+        return "Контакти";
+      default:
+        return "";
+    }
+  };
+
   return (
-    <div className="BreadCrumbs flex space-x-1 text-sm font-light">
-      <a
-        href={HOME_PAGE_ROUTE}
-        title="До головної"
-        className="hover:text-purple-main"
-      >
-        Головна
-      </a>
-      <ChevronRightIcon />
-      <div>{currentPage}</div>
+    <div className="BreadCrumbs text-sm font-light flex space-x-1">
+      {routes.map((route, index) => {
+        if (index < routes.length - 1) {
+          return (
+            <div className="flex space-x-1">
+              <a href={"/" + route} className="hover:text-purple-main">
+                {getName(route)}
+              </a>
+              <ChevronRightIcon />
+            </div>
+          );
+        }
+        return <div>{getName(route)}</div>;
+      })}
     </div>
   );
 };
