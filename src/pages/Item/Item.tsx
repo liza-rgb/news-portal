ba8ce1f30da3ba0e6a27a78320c89aed9939f913
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-06 02:14:59 +0100

    Added news summary to news and post item page

--- src/pages/Item/Item.tsx
+++ src/pages/Item/Item.tsx
@@ -6,6 +6,7 @@ import { convertToTranslit, findItemByTitle } from "../helpers/translitId";
 import PageTitle from "../../components/PageTitle";
 import TimeTag from "../../components/TimeTag";
 import NotFound from "../NotFound";
+import NewsSummary from "../../components/NewsSummary";
 
 export enum ItemType {
   NEWS = "news",
@@ -23,21 +24,28 @@ const Item: React.FC<ItemProps> = ({ type }) => {
   if (item) {
     return (
       <div className="NewsItem pt-5">
-        <PageTitle title={item.title} />
-        <div className="flex justify-between border-b-2 py-5 mb-5">
-          <a
-            href="/about"
-            className="text-sm text-purple-main underline"
-            title="Переглянути інформацію про автора"
-          >
-            {item.author}
-          </a>
-          <TimeTag
-            className="text-black-secondary opacity-50"
-            time={item.time}
-          />
+        <div className="grid grid-cols-11">
+          <div className="col-span-7">
+            <PageTitle title={item.title} />
+            <div className="flex justify-between border-b-2 py-5 mb-5">
+              <a
+                href="/about"
+                className="text-sm text-purple-main underline"
+                title="Переглянути інформацію про автора"
+              >
+                {item.author}
+              </a>
+              <TimeTag
+                className="text-black-secondary opacity-50"
+                time={item.time}
+              />
+            </div>
+            <p className="text-lg font-light">{item.description}</p>
+          </div>
+          <div className="col-span-4 ml-auto">
+            <NewsSummary />
+          </div>
         </div>
-        <p className="text-lg font-light">{item.description}</p>
       </div>
     );
   } else {
