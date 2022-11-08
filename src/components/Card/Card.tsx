db134bd681ebe60f483d72d8549159d9e9247b3f
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-08 14:19:41 +0100

    Added responsive behavior for Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -19,20 +19,25 @@ const Card: React.FC<CardProps> = ({ post }) => {
       title="Переглянути текст"
     >
       <div
-        className="Card max-w-[400px] space-y-3 hover:text-purple-main"
+        className="Card sm:max-w-[400px] space-y-3 hover:text-purple-main relative"
         id={post.id}
       >
         <img
           src={post.image}
           alt={post.title}
           title={post.title}
-          className="h-[246px] w-full object-cover"
+          className="xl:h-[246px] sm:h-[200px] h-[320px] w-full object-cover max-sm:brightness-50"
         />
-        <CategoryTag name={post.category} className="text-purple-main" />
-        <h4>{post.title}</h4>
-        <div className="flex justify-between text-black-secondary opacity-50">
-          <AuthorTag author={post.author} />
-          <TimeTag time={post.time} />
+        <div className="space-y-3 max-sm:absolute max-sm:bottom-[20px] max-sm:px-4">
+          <CategoryTag
+            name={post.category}
+            className="text-white sm:text-purple-main"
+          />
+          <h4 className="max-sm:text-white">{post.title}</h4>
+          <div className="flex justify-between text-white sm:text-black-secondary sm:opacity-50">
+            <AuthorTag author={post.author} />
+            <TimeTag time={post.time} />
+          </div>
         </div>
       </div>
     </a>
