e9558328cfae99df9cb6b80680fcaec5a5432bf7
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-17 04:17:59 +0100

    Fixed typo in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat: "Montserrat', sans-serif",
+        montserrat: "Montserrat, sans-serif",
       },
       colors: {
         transparent: "#00000000",
